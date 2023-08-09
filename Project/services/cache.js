564d139bc133f2b5c6cc7ca96f598f93ea43a12e
StephenDK <stephendklein08@gmail.com>
2023-08-08 23:03:05 -0700

    Cache Implementation

--- Project/services/cache.js
+++ Project/services/cache.js
@@ -1,3 +1,4 @@
+const { channel } = require("diagnostics_channel");
 const mongoose = require("mongoose");
 const redis = require("redis");
 const util = require("util");
@@ -12,17 +13,30 @@ client.get = util.promisify(client.get);
 
 const exec = mongoose.Query.prototype.exec;
 
-mongoose.Query.prototype.exec = function () {
+mongoose.Query.prototype.exec = async function () {
   console.log("IM ABOUT TO RUN A QUERY");
 
   //   console.log(this.getQuery());
   //   console.log(this.mongooseCollection.name);
 
-  const key = Object.assign({}, this.getQuery(), {
-    collection: this.mongooseCollection.name,
-  });
+  const key = JSON.stringify(
+    Object.assign({}, this.getQuery(), {
+      collection: this.mongooseCollection.name,
+    })
+  );
 
-  console.log(key);
+  //   console.log(key);
 
-  return exec.apply(this, arguments);
+  // See if we have a value for 'key' in redis
+  const cacheValue = await client.get(key);
+
+  // If we do, return value
+  if (cacheValue) {
+    console.log(cacheValue);
+  }
+
+  // Otherwise, issue the query and store the result in redis
+
+  const result = await exec.apply(this, arguments);
+  console.log(result);
 };
